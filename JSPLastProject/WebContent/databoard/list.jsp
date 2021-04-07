eda93542737b7860e5a51c761dfccf0a51fba7d5
SIST <SIST@DESKTOP-J6TRPCU>
2021-04-07 11:53:14 +0900

    1111

--- JSPLastProject/WebContent/databoard/list.jsp
+++ JSPLastProject/WebContent/databoard/list.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -30,6 +31,24 @@
 	      <th class="text-center" width=15%>이름</th>
 	      <th class="text-center" width=20%>작성일</th>
 	      <th class="text-center" width=10%>조회수</th>
+	     </tr>
+	     <c:forEach var="vo" items="${list }">
+	       <tr>
+		      <td class="text-center" width=10%>${vo.no }</td>
+		      <td class="text-left" width=45%>${vo.subject }</td>
+		      <td class="text-center" width=15%>${vo.name }</td>
+		      <td class="text-center" width=20%>${vo.regdate }</td>
+		      <td class="text-center" width=10%>${vo.hit }</td>
+	     </tr>
+	     </c:forEach>
+	   </table>
+	   <table class="table">
+	     <tr>
+	      <td class="text-center">
+	        <a href="#" class="btn btn-sm btn-success">이전</a>
+	          ${curpage } page / ${ totalpage} pages
+	        <a href="#" class="btn btn-sm btn-info">다음</a>
+	      </td>
 	     </tr>
 	   </table>
 	  </div>
--- JSPLastProject/src/com/sist/model/DataBoardModel.java
+++ JSPLastProject/src/com/sist/model/DataBoardModel.java
@@ -1,5 +1,7 @@
 package com.sist.model;
 
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 import com.sist.controller.Controller;
 import com.sist.controller.RequestMapping;
 
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.sist.dao.*;
 import com.sist.vo.*;
+import java.io.*;
 @Controller
 public class DataBoardModel {
   @RequestMapping("databoard/list.do")
@@ -35,6 +38,49 @@ public class DataBoardModel {
   {
 	  request.setAttribute("main_jsp", "../databoard/insert.jsp");
 	  return "../main/main.jsp";
+  }
+  @RequestMapping("databoard/insert_ok.do")
+  public String databoard_insert_ok(HttpServletRequest request,HttpServletResponse response)
+  {
+	  try
+	  {
+		  request.setCharacterEncoding("UTF-8");
+		  String path="c:\\upload\\";
+		  int size=1024*1024*100;
+		  String enctype="UTF-8";// 한글파일
+		  // 업로드 
+		  MultipartRequest mr=new MultipartRequest(request, path,size,enctype,
+				    new DefaultFileRenamePolicy());
+		  // 데이터 받기
+		  String name=mr.getParameter("name");
+		  String subject=mr.getParameter("subject");
+		  String content=mr.getParameter("content");
+		  String pwd=mr.getParameter("pwd");
+		  
+		  DataBoardVO vo=new DataBoardVO();
+		  vo.setName(name);
+		  vo.setSubject(subject);
+		  vo.setContent(content);
+		  vo.setPwd(pwd);
+		  
+		  String filename=mr.getOriginalFileName("upload");
+		  if(filename==null)
+		  {
+			  vo.setFilename("");
+			  vo.setFilesize(0);
+		  }
+		  else
+		  {
+			  File file=new File(path+filename);
+			  vo.setFilename(filename);
+			  vo.setFilesize((int)file.length());
+			  
+		  }
+		  //DB연동 
+		  DataBoardDAO dao=DataBoardDAO.newInstance();
+		  dao.databoardInsert(vo);
+	  }catch(Exception ex){}
+	  return "redirect:../databoard/list.do";
   }
 }
 
